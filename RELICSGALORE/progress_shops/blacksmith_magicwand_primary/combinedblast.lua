26df3a0c3903ee11900b0b0508756d8d06e07881
RER <rer@gmail.com>
2026-02-03 22:38:37 -0500

    removed frag grenade because it dont work

--- RELICSGALORE/progress_shops/blacksmith_magicwand_primary/combinedblast.lua
+++ RELICSGALORE/progress_shops/blacksmith_magicwand_primary/combinedblast.lua
@@ -1,23 +1,30 @@
 function ADVR.onLoad()
 	progress.name = "Combined blast"
-	progress.desc = "Casting uses all available orbs, that casted orbs damage scales both in size and damage with how many orbs were used"
-	progress.price = 3
+	progress.desc = "You have one mana orb, but it deals increased damage and knocks back enemies"
+	progress.price = 5
 	progress.currency = "beastblood"
 end
 
 function ADVR.ProgressEvents.onBuy()
 end
-function ADVR.onDungeonGenerated()
-  OrbsPlayerHas = game.inventory.currentSecondaryWeapon.AsMagicBookBase().maxMana
-end
-function ADVR.onWandCast(spawnedBullet)
-	spawnedBullet.transform.localScale = spawnedBullet.transform.localScale * (OrbsPlayerHas / 2)
-	 OrbsPlayerHas = OrbsPlayerHas - 1
-	 OrbsPlayerHas = 
-	
-end
-  
-	function ADVR.onWandCharged
-		 game.inventory.currentSecondaryWeapon.AsMagicOrbBase().orbLoadedParticles =  OrbsPlayerHas
+function ADVR.onDungeonGenerated(worldGenerator)
+  if helperMethods.IsInFirstDungeon(worldGenerator) then
+		game.inventory.currentSecondaryWeapon.AsMagicBookBase().SetMaxMana(1)
 	end
-	onorbreturn
+end
+
+function ADVR.onWandCast(spawnedBullet)
+spawnedBullet.transform.localScale = spawnedBullet.transform.localScale * 2
+end
+
+function ADVR.onRangedHitEntity(infos)
+   
+        if infos.entity == nil then
+            return infos.damage
+        end
+        if infos.entity.IsEnemy() then
+            infos.entity.AsEnemy().RpcKnockback(10, game.playerHeadset.transform.position, 10, knockbackType.FORCE_PUSH)
+        end
+    
+    return infos.damage
+end
