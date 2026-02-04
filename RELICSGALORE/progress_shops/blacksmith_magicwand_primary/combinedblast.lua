ecded8dd84b2342449e69927e002fd7552382c4c
RER <rer@gmail.com>
2026-02-03 22:39:58 -0500

    balances

--- RELICSGALORE/progress_shops/blacksmith_magicwand_primary/combinedblast.lua
+++ RELICSGALORE/progress_shops/blacksmith_magicwand_primary/combinedblast.lua
@@ -1,6 +1,6 @@
 function ADVR.onLoad()
 	progress.name = "Combined blast"
-	progress.desc = "You have one mana orb, but it deals increased damage and knocks back enemies"
+	progress.desc = "You have one mana orb, but it deals increased damage and knocks back enemies "
 	progress.price = 5
 	progress.currency = "beastblood"
 end
@@ -10,11 +10,13 @@ end
 function ADVR.onDungeonGenerated(worldGenerator)
   if helperMethods.IsInFirstDungeon(worldGenerator) then
 		game.inventory.currentSecondaryWeapon.AsMagicBookBase().SetMaxMana(1)
-	end
+    end
+    player.PrimaryDamage.RegisterAddend(progress.id, 3)
 end
 
+
 function ADVR.onWandCast(spawnedBullet)
-spawnedBullet.transform.localScale = spawnedBullet.transform.localScale * 2
+spawnedBullet.transform.localScale = spawnedBullet.transform.localScale * 2.5
 end
 
 function ADVR.onRangedHitEntity(infos)
@@ -23,7 +25,7 @@ function ADVR.onRangedHitEntity(infos)
             return infos.damage
         end
         if infos.entity.IsEnemy() then
-            infos.entity.AsEnemy().RpcKnockback(10, game.playerHeadset.transform.position, 10, knockbackType.FORCE_PUSH)
+            infos.entity.AsEnemy().RpcKnockback(5, game.playerHeadset.transform.position, 10, knockbackType.FORCE_PUSH)
         end
     
     return infos.damage
