faf8cb4b911250fb450ed1916ef8cc48a77d92de
RER <arijacob09@icloud.com>
2026-02-06 18:41:49 -0500

    Finished chefs mark

--- RELICSGALORE/items/chefs_mark/chefs_mark.lua
+++ RELICSGALORE/items/chefs_mark/chefs_mark.lua
@@ -5,13 +5,16 @@ function ADVR.onLoad()
 	pickup.maxAmount = 1
 	pickup.price = 30
 	pickup.tier = 2
-	pickup.spawnsIn = {relicPool.SHOP, relicPool.BOSS, relicPool.Golden_chest}
+	pickup.spawnsIn = {relicPool.SHOP, relicPool.BOSS, relicPool.GOLDEN_CHEST}
 	pickup.supportedInMultiplayer = true
-    pickup.isactiveitem = true
+    pickup.isActivateItem = true
 end
 
 function ADVR.onPickup()
 pickup.RegisterItem()
 
 end
-
+function ADVR.onPickupActivate()
+local food = game.SpawnObjectNetwork(objects.ITEM_APPLE, game.playerController.rightHand.transform.position)
+	food.GetComponent_StartHoverWhenLowVelocity_().SetDistanceOverGround(1.1, 0)
+end
