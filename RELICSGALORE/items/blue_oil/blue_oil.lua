e885c6bd7ba396d3cd80a184ca4bb15fad01185d
RER <arijacob09@icloud.com>
2026-02-25 22:36:16 -0500

    Fixed both magenta and blue oil because i might be stupid

--- RELICSGALORE/items/blue_oil/blue_oil.lua
+++ RELICSGALORE/items/blue_oil/blue_oil.lua
@@ -12,6 +12,13 @@ end
 
 
 function ADVR.onPickup()
+	
+	base.preObjectSpawnListeners = {
+		game.GetObjectMapping(objects.ITEM_FOOD_APPLE),
+		game.GetObjectMapping(objects.ITEM_FOOD_POTATO),
+		game.GetObjectMapping(objects.ITEM_FOOD_STEAK),
+		game.GetObjectMapping(objects.ITEM_FOOD_DRAGONFRUIT)
+	}
     pickup.RegisterItem()
     ProbabilityPotatoReplace = 0.80
 	ProbabilityAppleReplace = 0.2
@@ -19,6 +26,13 @@ function ADVR.onPickup()
 	ProbabilityDragonfruitReplace = 0.0
 end
 function ADVR.onPickupProxies(originalPlayerRef)
+	
+	base.preObjectSpawnListeners = {
+		game.GetObjectMapping(objects.ITEM_FOOD_APPLE),
+		game.GetObjectMapping(objects.ITEM_FOOD_POTATO),
+		game.GetObjectMapping(objects.ITEM_FOOD_STEAK),
+		game.GetObjectMapping(objects.ITEM_FOOD_DRAGONFRUIT)
+	}
 	pickup.RegisterItemNoSync()
     ProbabilityPotatoReplace = 0.8
 	ProbabilityAppleReplace = 0.2
@@ -27,3 +41,53 @@ function ADVR.onPickupProxies(originalPlayerRef)
 end
 
 
+
+function ADVR.onPreObjectSpawn(prefab, override)
+    -- Apple
+    if prefab == game.GetObjectMapping(objects.ITEM_FOOD_APPLE) then
+        if math.random() < ProbabilityAppleReplace then
+            local replacements = {
+                game.GetObjectMapping(objects.ITEM_FOOD_PLUM),
+                game.GetObjectMapping(objects.ITEM_FOOD_CHEESE),
+                game.GetObjectMapping(objects.ITEM_FOOD_PEAR),
+                game.GetObjectMapping(objects.ITEM_FOOD_ORANGE)
+            }
+            -- Randomly pick one replacement
+            return replacements[math.random(#replacements)]
+        end
+    end
+
+    -- Potato
+    if prefab == game.GetObjectMapping(objects.ITEM_FOOD_POTATO) then
+        if math.random() < ProbabilityPotatoReplace then
+            local replacements = {
+                game.GetObjectMapping(objects.ITEM_FOOD_YAM),
+                game.GetObjectMapping(objects.ITEM_FOOD_BEET)
+            }
+            return replacements[math.random(#replacements)]
+        end
+    end
+
+    -- Steak
+    if prefab == game.GetObjectMapping(objects.ITEM_FOOD_STEAK) then
+        if math.random() < ProbabilitySteakReplace then
+            local replacements = {
+                game.GetObjectMapping(objects.ITEM_FOOD_CHICKEN)
+            }
+            return replacements[math.random(#replacements)]
+        end
+    end
+
+    -- Dragonfruit
+    if prefab == game.GetObjectMapping(objects.ITEM_FOOD_DRAGONFRUIT) then
+        if math.random() < ProbabilityDragonfruitReplace then
+            local replacements = {
+            }
+            if #replacements > 0 then
+                return replacements[math.random(#replacements)]
+            end
+        end
+    end
+
+    return override
+end
--- RELICSGALORE/items/magenta_oil/magenta_oil.lua
+++ RELICSGALORE/items/magenta_oil/magenta_oil.lua
@@ -27,3 +27,53 @@ function ADVR.onPickupProxies(originalPlayerRef)
 end
 
 
+
+function ADVR.onPreObjectSpawn(prefab, override)
+    -- Apple
+    if prefab == game.GetObjectMapping(objects.ITEM_FOOD_APPLE) then
+        if math.random() < ProbabilityAppleReplace then
+            local replacements = {
+                game.GetObjectMapping(objects.ITEM_FOOD_PLUM),
+                game.GetObjectMapping(objects.ITEM_FOOD_CHEESE),
+                game.GetObjectMapping(objects.ITEM_FOOD_PEAR),
+                game.GetObjectMapping(objects.ITEM_FOOD_ORANGE)
+            }
+            -- Randomly pick one replacement
+            return replacements[math.random(#replacements)]
+        end
+    end
+
+    -- Potato
+    if prefab == game.GetObjectMapping(objects.ITEM_FOOD_POTATO) then
+        if math.random() < ProbabilityPotatoReplace then
+            local replacements = {
+                game.GetObjectMapping(objects.ITEM_FOOD_YAM),
+                game.GetObjectMapping(objects.ITEM_FOOD_BEET)
+            }
+            return replacements[math.random(#replacements)]
+        end
+    end
+
+    -- Steak
+    if prefab == game.GetObjectMapping(objects.ITEM_FOOD_STEAK) then
+        if math.random() < ProbabilitySteakReplace then
+            local replacements = {
+                game.GetObjectMapping(objects.ITEM_FOOD_CHICKEN)
+            }
+            return replacements[math.random(#replacements)]
+        end
+    end
+
+    -- Dragonfruit
+    if prefab == game.GetObjectMapping(objects.ITEM_FOOD_DRAGONFRUIT) then
+        if math.random() < ProbabilityDragonfruitReplace then
+            local replacements = {
+            }
+            if #replacements > 0 then
+                return replacements[math.random(#replacements)]
+            end
+        end
+    end
+
+    return override
+end
