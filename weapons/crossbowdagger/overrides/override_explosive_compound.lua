f32d5926c8c2b0d175bcb3af1e7fc1b3ef477453
RER <rer@gmail.com>
2026-03-22 14:09:17 -0400

    Tried to fix explosive compound, but just changed itrs function for cbow

--- weapons/crossbowdagger/overrides/override_explosive_compound.lua
+++ weapons/crossbowdagger/overrides/override_explosive_compound.lua
@@ -1,6 +1,6 @@
 function ADVR.onLoad()
     pickup.name = "Explosive compound"
-    pickup.desc = "Melee attacks cause an explosion, but melee damage is decreased"
+    pickup.desc = "Punch attacks cause an explosion, but melee damage is decreased"
     pickup.supportedInMultiplayer = true
     game.RegisterForRPCEvents(pickup)
 end
@@ -9,7 +9,7 @@ function ADVR.onPickup()
     helperMethods.RegisterMeleeDamageAddMult(pickup.id, 1, -1.8)
 end
 
-function ADVR.onMeleeWeaponCollide(WTFDOIPUTHERE, collision)
+function ADVR.onMeleeWeaponCollide(sword, collision)
 	-- special case for chests, which should just open naturally 
 	local living = collision.gameObject.GetComponent_LivingBase_()
 	if living ~= nil and string.find(living.livingId, "prop_chest_") then
@@ -18,16 +18,17 @@ function ADVR.onMeleeWeaponCollide(WTFDOIPUTHERE, collision)
 	end
 
 	if collision.relativeVelocity.magnitude > 2 then
-		local expl = game.SpawnObjectLocal(objects.EFFECT_EXPLOSION_SMALL, player.Object.WTFDOPUTHERE_.transform.position).GetComponent_Explosion_()
+		local expl = game.SpawnObjectLocal(objects.EFFECT_EXPLOSION_SMALL,  sword.transform.position).GetComponent_Explosion_()
 		expl.damage = player.GetSecondaryDamageWithPickupModifiers()
 		expl.enableCritChance = true
-		pickup.SendRPCEvent(1, player.Object.swor.transform.position, rpcType.TO_PROXIES)
+		pickup.SendRPCEvent(1, sword.transform.position, rpcType.TO_PROXIES)
 	end
 end
 
-function ADVR.onRPCReceived(eventID, sourcePlayer, position)
+function ADVR.onRPCReceived(eventID, position)
 	if eventID == 1 then
 		local expl = game.SpawnObjectLocal(objects.EFFECT_EXPLOSION_SMALL, position).GetComponent_Explosion_()
 		expl.damage = 0
 	end
 end
+ 
