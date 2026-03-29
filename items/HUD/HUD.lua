4a01df30c54963f851b73370e7f671a523171ba3
RER <rer@gmail.com>
2026-03-28 21:20:24 -0400

    nerfed HUD

--- items/HUD/HUD.lua
+++ items/HUD/HUD.lua
@@ -5,8 +5,8 @@ function ADVR.onLoad()
     pickup.maxAmount = 1
     pickup.amountUses = -1
     pickup.price = 40
-    pickup.tier = 2
-    pickup.spawnsIn = {relicPool.PODEST, relicPool.BOSS, relicPool.SPECIAL}
+    pickup.tier = 3
+    pickup.spawnsIn = {relicPool.PODEST, relicPool.SPECIAL}
     pickup.supportedInMultiplayer = true
 end
 
