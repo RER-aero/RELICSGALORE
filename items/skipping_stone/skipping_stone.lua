fe2a7e5531526b6bb381f977829e82922daad005
RER <rer@gmail.com>
2026-04-04 23:42:13 -0400

    added skipping stone

---
+++ items/skipping_stone/skipping_stone.lua
@@ -0,0 +1,29 @@
+
+function ADVR.onLoad()
+    pickup.name = "Skipping Stone"
+    pickup.desc = "Increase stats, but taking relics will slow you down"
+	pickup.weight = 75
+	pickup.maxAmount = 1
+	pickup.amountUses = -1
+	pickup.price = 50
+	pickup.tier = 2
+	pickup.spawnsIn = {relicPool.SPECIAL, relicPool.BOSS}
+	pickup.supportedInMultiplayer = true
+end
+
+function ADVR.onPickup()
+	pickup.RegisterItem()
+
+	player.PrimaryDamage.RegisterAddend(pickup.id, 4)
+	player.PrimaryDamage.RegisterMultiplier(pickup.id, 1.2)
+	player.PrimaryCritChance.RegisterAddend(pickup.id, 0.15)
+	player.SecondaryCritChance.RegisterAddend(pickup.id, 0.15)
+	player.SecondaryDamage.RegisterAddend(pickup.id, 2)
+	player.EvasionChance.RegisterAddend(pickup.id, 0.15)
+end
+
+
+function ADVR.onPickupTaken(takenPickup)
+	player.MoveSpeed.RegisterAddend(pickup.id, -10)
+
+end
