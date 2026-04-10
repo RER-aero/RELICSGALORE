a2538df0ab121903d4268381ea8628f294ee01d7
RER <rer@gmail.com>
2026-04-10 17:33:02 -0400

    added caviar

---
+++ items/caviar/caviar.lua
@@ -0,0 +1,23 @@
+function ADVR.onLoad()
+    pickup.name = "Caviar"
+    pickup.desc = "Eating food has a chance to reward keys"
+	pickup.weight = 35
+	pickup.maxAmount = 3
+	pickup.price = 20
+	pickup.tier = 1
+	pickup.spawnsIn = {relicPool.SHOP, relicPool.SPECIAL}
+	pickup.supportedInMultiplayer = true
+	pickup.isSharedItem = true
+end
+
+function ADVR.onPickupProxies(originalPlayerRef)
+	pickup.RegisterItemNoSync()
+end
+
+function ADVR.onPickup()
+	pickup.RegisterItem()
+end
+
+function ADVR.onFoodEaten(food)
+	player.CurrentKeys = player.CurrentKeys + food.healAmount * 4 * pickup.AmountActiveLocal()
+end
