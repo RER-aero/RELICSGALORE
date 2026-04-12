ac5cba35defed10d35953918451b0eeae98f1571
RER <rer@gmail.com>
2026-04-12 18:53:17 -0400

    added golden yoyo

---
+++ items/golden_yoyo/golden_yoyo.lua
@@ -0,0 +1,25 @@
+function ADVR.onLoad()
+    pickup.name = "Golden YoYo"
+    pickup.desc = "Gain extra scrying coins if you win this run"
+	pickup.weight = 55
+	pickup.maxAmount = 3
+	pickup.amountUses = -1
+	pickup.price = 35
+	pickup.tier = 1
+	pickup.spawnsIn = {relicPool.SHOP}
+	pickup.supportedInMultiplayer = true
+end
+
+function ADVR.onPickup()
+	pickup.RegisterItem()
+    	
+end
+
+
+function ADVR.onRunComplete()
+	
+        local rand = math.random(5,8)
+        
+		player.GrandLibraryCoins = player.GrandLibraryCoins + rand + player.newGamePlusLevel
+        
+	end
