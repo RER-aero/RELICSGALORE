59403e1938091055bc870e5723c50ba7449c1fe9
RER <rer@gmail.com>
2026-04-12 14:44:52 -0400

    added soul in a jar

--- items/soul_in_a_jar/soul_in_a_jar.lua
+++ items/soul_in_a_jar/soul_in_a_jar.lua
@@ -1,6 +1,6 @@
 function ADVR.onLoad()
     pickup.name = "Soul in a Jar"
-    pickup.desc = "Revive ONCE"
+    pickup.desc = "Revive once, but loose heart containers and revive with 1 protection heart"
     pickup.weight = 10
     pickup.maxAmount = 1
     pickup.amountUses = -1
@@ -18,6 +18,8 @@ function ADVR.onPlayerHit(damage, damageSource, receivedDamageType, hitPosition,
     if not isStatsProbe and damage >= player.RealHealth then
         pickup.UnregisterItem()
         audio.PlaySoundLocal(sounds.DEATH_REVIVE_FOOD_EAT_FROM_SLOT, player.transform.position)
+         player.AddEtherealHealth(2, game.itemInterpreter.GetItemById(relics.BELOVED_KEEPSAKE))	
+	player.MaxHealth = 0
         return 0
     end
 
