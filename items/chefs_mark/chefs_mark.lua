2a991e3e877bca601a35afb9f9e47165719ea3d4
RER <rer@gmail.com>
2026-04-15 21:54:26 -0400

    fixed chefs mark

--- items/chefs_mark/chefs_mark.lua
+++ items/chefs_mark/chefs_mark.lua
@@ -4,6 +4,7 @@ function ADVR.onLoad()
 	pickup.weight = 70
 	pickup.maxAmount = 1
 	pickup.price = 30
+	pickup.amountUses = 12
 	pickup.tier = 2
 	pickup.spawnsIn = {relicPool.SHOP, relicPool.BOSS, relicPool.GOLDEN_CHEST}
 	pickup.supportedInMultiplayer = true
@@ -11,9 +12,20 @@ function ADVR.onLoad()
 end
 
 function ADVR.onPickup()
-pickup.RegisterItem()
+pickup.RegisterUsable()
 end
+
 function ADVR.onPickupActivate()
-local food = game.SpawnObjectNetwork(objects.ITEM_APPLE, game.playerController.rightHand.transform.position)
+	local epstein = math.random(4)
+	local food = game.SpawnObjectNetwork(objects.ITEM_FOOD_POTATO, game.playerController.rightHand.transform.position)
+	if epstein == 1 then
+ food = game.SpawnObjectNetwork(objects.ITEM_APPLE, game.playerController.rightHand.transform.position)
+	elseif epstein == 2 then
+ food = game.SpawnObjectNetwork(objects.ITEM_FOOD_STEAK, game.playerController.rightHand.transform.position)
+	elseif epstein == 3 then
+		 food = game.SpawnObjectNetwork(objects.ITEM_FOOD_CHICKEN, game.playerController.rightHand.transform.position)
+	else 
+		 food = game.SpawnObjectNetwork(objects.ITEM_FOOD_CHEESE, game.playerController.rightHand.transform.position)
+	end
 	food.GetComponent_StartHoverWhenLowVelocity_().SetDistanceOverGround(1.1, 0)
 end
