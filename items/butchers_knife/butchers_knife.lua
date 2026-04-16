793ddfb7b834c477fede0c1a072506a4cfc07547
RER <rer@gmail.com>
2026-04-15 23:20:42 -0400

    WHAT THE FUCK IS WRONG WITH THIS

--- items/butchers_knife/butchers_knife.lua
+++ items/butchers_knife/butchers_knife.lua
@@ -1,42 +1,42 @@
 function ADVR.onLoad()
-	pickup.name = "Butchers knife"
+    pickup.name = "Butchers Knife"
     pickup.desc = "All of your current and future bone related relics increase your melee damage by 1"
-	pickup.weight = 65
-	pickup.maxAmount = 1
-	pickup.price = 35
-	pickup.tier = 3
-	pickup.spawnsIn = { relicPool.SPECIAL}
-	pickup.supportedInMultiplayer = true
+    pickup.weight = 65
+    pickup.maxAmount = 1
+    pickup.price = 35
+    pickup.tier = 3
+    pickup.spawnsIn = { relicPool.SPECIAL }
+    pickup.supportedInMultiplayer = true
 end
+
 RelicsToCheck = {
     relics.BONE_DICE,
     relics.BLUNTED_KNUCKLEBONE,
     relics.PACKRAT_MANDIBLE,
     relics.ASCETICS_MARK,
     relics.GOLD_TOOTH,
-    relics.SNAKE_FANG,
 }
 function ADVR.onPickup()
-pickup.RegisterItem()
+    pickup.RegisterItem()
 
 
-local count = 0
-for i = 1, #RelicsToCheck do
-    if game.itemInterpreter.AmountPickupFoundByLocalPlayer(RelicsToCheck[i]) > 0 then
-        count = count + 1
+    local count = 0
+    local items = game.itemInterpreter.GetActivePickupsFromAllPlayers()
+    for i = 1, #RelicsToCheck do
+        for j = 1, #RelicsToCheck do
+            if items[i] == RelicsToCheck[j] then
+                count = count + 1
+            end
+        end
+
+        helperMethods.RegisterMeleeDamageAddend(pickup.id, count)
     end
 end
 
-helperMethods.RegisterMeleeDamageAddend(pickup.id, count)
-end
-function ADVR.onPickupTaken(itemUpgrade) 
-for i = 1, #RelicsToCheck do
-    if itemUpgrade == RelicsToCheck[i] then
-     helperMethods.RegisterMeleeDamageAddend(pickup.id, 1)
+function ADVR.onPickupTaken(itemUpgrade)
+    for i = 1, #RelicsToCheck do
+        if itemUpgrade == RelicsToCheck[i] then
+            helperMethods.RegisterMeleeDamageAddend(pickup.id, 1)
+        end
     end
 end
-
-end
-
-
-
