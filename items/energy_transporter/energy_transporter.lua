6abb1cbc167bf32d6f7e9e380d2d80b02d2b4488
RER-aero <arijacob09@icloud.com>
2026-05-07 19:06:05 -0400

    added energy transporter

---
+++ items/energy_transporter/energy_transporter.lua
@@ -0,0 +1,19 @@
+function ADVR.onLoad()
+    pickup.name = "Energy Transporter"
+    pickup.desc = "Picking up relics recharges your current mark slightly"
+    pickup.weight = 90
+    pickup.maxAmount = 1
+    pickup.amountUses = -1
+    pickup.price = 20
+    pickup.tier = 1
+    pickup.spawnsIn = { relicPool.PODEST }
+    pickup.supportedInMultiplayer = true
+end
+
+function ADVR.onPickup()
+    pickup.RegisterItem()
+    if game.itemInterpreter.currentUsable ~= nil then
+        game.itemInterpreter.currentUsable.currentCharge = game.itemInterpreter.currentUsable.currentCharge + 3
+        game.activePickupSlot.UpdateChargeDisplay()
+    end
+end
