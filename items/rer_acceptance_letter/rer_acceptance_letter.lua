7cd5f9d2a1b391f9a4a531af7c6489c59a87885f
RER-aero <arijacob09@icloud.com>
2026-06-12 14:16:58 -0400

    guh

--- items/rer_acceptance_letter/rer_acceptance_letter.lua
+++ items/rer_acceptance_letter/rer_acceptance_letter.lua
@@ -1,23 +1,22 @@
 function ADVR.onLoad()
-    pickup.name = "Acceptance Letter"
-    pickup.desc = "Chance to gain or lose a heart container"
+	pickup.name = "Acceptance Letter"
+	pickup.desc = "Chance to gain or lose a heart container"
 	pickup.weight = 90
-	pickup.tier = 2
+	pickup.tier = 1
 	pickup.maxAmount = 10
 	pickup.amountUses = -1
 	pickup.price = 25
-	pickup.spawnsIn = {relicPool.GOLDEN_CHEST, relicPool.SHOP, relicPool.SPECIAL}
+	pickup.spawnsIn = { relicPool.GOLDEN_CHEST, relicPool.SHOP, relicPool.SPECIAL }
 	pickup.supportedInMultiplayer = true
 end
 
 function ADVR.onPickup()
-  
-    if helperMethods.IsValidWithLuck(.50,.50,.50) then
-	player.MaxHealth = player.MaxHealth + 2
-	player.Health = player.Health + 2
+	if helperMethods.IsValidWithLuck(.50, .50, .50) then
+		player.MaxHealth = player.MaxHealth + 2
+		player.Health = player.Health + 2
 	else
-    player.MaxHealth = player.MaxHealth - 2
-end
+		player.MaxHealth = player.MaxHealth - 2
+	end
 
 	pickup.RegisterItem()
 end
